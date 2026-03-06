6b6243a2d2a836571c48a46317956aefb9dbddcb
athenalingit <atl009@ucsd.edu>
2026-03-06 11:59:59 -0800

    push current change

--- imports.py
+++ imports.py
@@ -10,3 +10,7 @@ mne.set_log_level("WARNING")
 # load all data file paths into eeg_files
 data_path = Path("ds004504-download")
 eeg_files = sorted(list(data_path.glob("sub-*/eeg/*.set")))
+
+# each subject load as a MNR raw object
+file = eeg_files[0]
+raw = mne.io.read_raw_eeglab(file, preload=True)
